cc65899f0a09ef298f2dec633236b4bbcd66f4c5
keshaxgit <egortocenov2@mail.ru>
2025-06-28 19:52:12 +0300

    second commit

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,11 @@
-
 export default defineNuxtConfig({
-  compatibilityDate: '2025-05-15',
-  devtools: { enabled: true }
-
-  
-})
+  ssr: true, // или false, если хотите чисто статический сайт
+  app: {
+    baseURL: '/repository-name/', // если репозиторий USERNAME.github.io, то оставьте '/'
+  },
+  nitro: {
+    prerender: {
+      crawlLinks: true, // для генерации всех страниц
+    },
+  },
+});
